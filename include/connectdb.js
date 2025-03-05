0418e5f3e7e1167547365aa8ceb8380f2601c521
Gabriel Russo <tenbu64@proton.me>
2025-03-05 12:18:08 -0300

    edit to callback style

--- include/connectdb.js
+++ include/connectdb.js
@@ -1,15 +1,10 @@
 import pg from "pg";
 const { Client } = pg;
 const client = new Client();
-await client.connect();
 
-try {
-  const res = await client.query("SELECT $1::text as message", [
-    "Hello world!",
-  ]);
-  console.log(res.rows[0].message); // Hello world!
-} catch (err) {
-  console.error(err);
-} finally {
-  await client.end();
-}
+client.connect((err) => {
+  client.query("SELECT $1::text as message", ["Hello world!"], (err, res) => {
+    console.log(err ? err.stack : res.rows[0].message); // Hello World!
+    client.end();
+  });
+});
