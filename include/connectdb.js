031e9750dd015dd09c2dbcaf064056abca19f1e4
Gabriel Russo <tenbu64@proton.me>
2025-03-05 00:04:22 -0300

    installing pg && adding connectdb.js

---
+++ include/connectdb.js
@@ -0,0 +1,15 @@
+import pg from "pg";
+const { Client } = pg;
+const client = new Client();
+await client.connect();
+
+try {
+  const res = await client.query("SELECT $1::text as message", [
+    "Hello world!",
+  ]);
+  console.log(res.rows[0].message); // Hello world!
+} catch (err) {
+  console.error(err);
+} finally {
+  await client.end();
+}
