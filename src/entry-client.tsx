fdac69aa9fecb7482571ae4e33d6cf52b1074b94
Gabriel Russo <tenbu64@proton.me>
2025-03-06 20:20:19 -0300

    readd index

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,11 +1,11 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { hydrateRoot } from 'react-dom/client'
-import App from './App'
+import "./css/index.css";
+import { StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import App from "./App";
 
 hydrateRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById("root") as HTMLElement,
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
