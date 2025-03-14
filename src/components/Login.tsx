cec62ef4637a6dd15fe66aa7619044f5ea41203e
Gabriel Russo <tenbu64@proton.me>
2025-03-13 22:34:29 -0300

    refactor login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,18 @@
 import "../css/App.css";
-import { dbInsertQuery } from "../db/DBConnect.js";
+
 export default function Login() {
+  function handleSubmit(formData: any) {
+    const emailData = formData.get("email");
+    const passwdData = formData.get("passwd");
+    dbInsertQuery;
+    // console.log(`You searched for ${emailData} and ${passwdData}`);
+  }
   return (
     <>
       {" "}
       <div className="login-wrapper">
         <h1>Please Log In</h1>
-        <form id="loginForm" action={dbInsertQuery}>
+        <form id="loginForm" action={handleSubmit}>
           <label>
             <span id="usernameInfo" className="loginInfo">
               <input name="email" type="text" placeholder="e-mail" />
