d7ae714a6d71d6ab8979cdca8a6863e652afa078
Gabriel Russo <tenbu64@proton.me>
2025-04-03 16:53:35 -0300

    second commit - base stuff

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import Home from "./Home.tsx";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
-);
