d5af24367c3c994a789503eee071f74cd25e2061
Gabriel Russo <tenbu64@proton.me>
2025-04-13 13:50:38 -0300

    diabdat

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
